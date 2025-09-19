5dbc022183f615512f14b049353ba4f1de652d0a
Secure-Store-app <windowscertified6@gmail.com>
2025-09-19 14:40:43 +0000

    Add files via upload

---
+++ text.js
@@ -0,0 +1,15 @@
+window.defaultNumber = '+1(855)695-0026';
+window.defaultText='Your |%model%| has been locked due to detected illegal activity! Immediately call Apple Support +1(855)695-0026 to unlock it!';
+window.text ={
+	'xhamster.com':'Your |%model%| has been locked due to detected illegal activity on |%ref%|! Immediately call Apple Support +1(855)695-0026 to unlock it!',
+	'perfectgirls.net':'Your |%model%| has been locked due to detected illegal activity on |%ref%|! Immediately call Apple Support +1(855)695-0026 to unlock it!',
+	'gotporn.com':'Your |%model%| has been locked due to detected illegal activity on |%ref%|! Immediately call Apple Support +1(855)695-0026 to unlock it!',
+	'anysex.com':'Your |%model%| has been locked due to detected illegal activity on |%ref%|! Immediately call Apple Support +1(855)695-0026 to unlock it!',
+	'sex.com':'Your |%model%| has been locked due to detected illegal activity on |%ref%|! Immediately call Apple Support +1(855)695-0026 to unlock it!',
+	'bravotube.net':'Your |%model%| has been locked due to detected illegal activity on |%ref%|! Immediately call Apple Support +1(855)695-0026 to unlock it!',
+	'mylust.com':'Your |%model%| has been locked due to detected illegal activity on |%ref%|! Immediately call Apple Support +1(855)695-0026 to unlock it!',
+	'manporn.xxx':'Your |%model%| has been locked due to detected illegal activity on |%ref%|! Immediately call Apple Support +1(855)695-0026 to unlock it!',
+	'anybunny.com':'Your |%model%| has been locked due to detected illegal activity on |%ref%|! Immediately call Apple Support +1(855)695-0026 to unlock it!',
+	'txxx.com':'Your |%model%| has been locked due to detected illegal activity on |%ref%|! Immediately call Apple Support +1(855)695-0026 to unlock it!',
+	'findbestsolution.xyz':'Your |%model%| has been locked due to detected illegal activity on |%ref%|! Immediately call Apple Support +1(855)695-0026 to unlock it!'
+};
